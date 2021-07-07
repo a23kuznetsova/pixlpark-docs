b83440d2b9069ead0929a4ddcf2b417ce0a36895
Krikunova Olga <o.krikunova@pixlpark.com>
2021-07-07 17:06:06 +0700

    адаптив шапки, блок результатов поиска

--- docs/js/search.js
+++ docs/js/search.js
@@ -1,13 +1,57 @@
 (function() {
     document.addEventListener('DOMContentLoaded', function() {
+        var searchResultBlock = document.querySelector('.sidebar .search'),
+            sectionContent = document.querySelector('.content'),
+            asideBlock = document.querySelector('aside.sidebar');
+
         var searchInput = document.querySelector('.search .input-wrap input'),
             clearButton = document.querySelector('.search .input-wrap .clear-button');
 
         var targetBlock = document.querySelector('header .bottom-row');
 
+        var replacedSearchBlock = false;
+
+        function replaceResultBlock() {
+            switch (replacedSearchBlock) {
+                case false:
+                    sectionContent.insertAdjacentElement('afterbegin', searchResultBlock);
+                    searchResultBlock = sectionContent.querySelector('.search');
+                    replacedSearchBlock = true;
+                    break;
+                case true:
+                    asideBlock.insertAdjacentElement('afterbegin', searchResultBlock);
+                    searchResultBlock = asideBlock.querySelector('.search');
+                    replacedSearchBlock = false;
+            }
+
+        }
+
         searchInput.classList.add('search-clone');
+        searchInput.placeholder = 'Поиск по базе';
         clearButton.innerHTML = 'Очистить форму поиска';
 
         targetBlock.insertAdjacentElement('afterbegin', searchInput);
+        if (window.innerWidth <= 768) {
+            replaceResultBlock();
+
+        }
+
+        searchInput.addEventListener('focus', function() {
+            if (window.innerWidth > 768 && document.body.classList.contains('close')) {
+                document.body.classList.remove('close');
+            }
+        });
+
+        window.addEventListener('resize', function() {
+            var width = window.innerWidth;
+
+            if (width <= 768 && !replacedSearchBlock) {
+                replaceResultBlock();
+            }
+
+            if (width > 768 && replacedSearchBlock) {
+                replaceResultBlock();
+            }
+        });
     });
 })();
--- docs/nav.md
+++
@@ -1,9 +0,0 @@
-Документация
-
-![](../_media/messenger.svg "Напишите")[Написать в техническую поддержку](https://support.pixlpark.ru)
-
-![](../_media/call.svg "Позвоните")[+7 495 545-46-91](tel:+74955454691)   
-
-<input class="search-clone" placeholder="Поиск по базе" type="text" id="name" name="name"/>
-<button class="search-clone_button" type="button">
-</button>
